b5a4efbe366c7dfc82be8533c7c8e7322fe5a338
Vince-Diversity <vince.diversity@gmail.com>
2024-01-03 14:10:15 +0100

    Removed v0.2 from the dev file, so godot doesn't get confused.

--- dev/v02archive/text_box/narrative_label.gd
+++
@@ -1,6 +0,0 @@
-extends "res://addons/dialogue_manager/dialogue_label.gd"
-
-func type_out() -> void:
-	.type_out()
-	if dialogue.character != "":
-		bbcode_text = "%s: %s" % [dialogue.character, bbcode_text]
