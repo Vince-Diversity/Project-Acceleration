804ed36a06293862f305d5321011f1b50b7d44b6
Vince-Diversity <vince.diversity@gmail.com>
2024-03-24 10:01:20 +0100

    Made cutscene for entering the stairs going to the imaginary. Fixed a condition.

--- loader/controls.gd
+++ loader/controls.gd
@@ -2,4 +2,4 @@ extends RichTextLabel
 
 func _ready():
 	text = "Version %s\n" % ProjectSettings.get_setting("application/config/version")
-	append_text("Controls: Arrow keys, X, Z, Esc and you'll see...\n")
+	append_text("Controls: Arrow keys, Z, S or Esc and you'll see more later...\n")
